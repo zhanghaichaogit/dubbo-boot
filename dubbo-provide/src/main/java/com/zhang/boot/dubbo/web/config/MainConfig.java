68dfff7b5dc6c53b6d4030403bdbcdbd462541cf
zhanghaichao <704877313@qq.com>
2017-10-22 20:31:14 +0800

    提交下修改

--- dubbo-provide/src/main/java/com/zhang/boot/dubbo/web/config/MainConfig.java
+++ dubbo-provide/src/main/java/com/zhang/boot/dubbo/web/config/MainConfig.java
@@ -2,11 +2,13 @@ package com.zhang.boot.dubbo.web.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by zhanghaichao on 2017/10/17.
  */
 @SpringBootApplication
+@ImportResource({ "classpath:config/bean.xml" })
 public class MainConfig {
     public static void main(String[] args) {
         SpringApplication.run(MainConfig.class, args);
--- dubbo-provide/src/main/java/com/zhang/boot/dubbo/web/serviceImpl/DubboserviceImpl.java
+++ dubbo-provide/src/main/java/com/zhang/boot/dubbo/web/serviceImpl/DubboserviceImpl.java
@@ -1,12 +1,10 @@
 package com.zhang.boot.dubbo.web.serviceImpl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.zhang.boot.dubbo.api.service.ServiceTest;
 
 /**
  * Created by zhanghaichao on 2017/10/17.
  */
-@Service
 public class DubboserviceImpl implements ServiceTest {
 
     @Override
