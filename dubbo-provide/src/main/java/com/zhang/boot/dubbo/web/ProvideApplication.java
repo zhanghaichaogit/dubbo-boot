f14d7bb2cc791bd9025271a2c05494ca4374bfea
张海超 <zhanghaichao@yingxinhuitong.com>
2017-10-27 17:04:28 +0800

    靠谱的版本

--- dubbo-provide/src/main/java/com/zhang/boot/dubbo/web/ProvideApplication.java
+++ dubbo-provide/src/main/java/com/zhang/boot/dubbo/web/ProvideApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @EnableAutoConfiguration
 ////引入spring配置文件
-@ImportResource({"classpath:config/bean.xml"})
+@ImportResource({"classpath:config/spring-config.xml"})
 public class ProvideApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProvideApplication.class, args);
